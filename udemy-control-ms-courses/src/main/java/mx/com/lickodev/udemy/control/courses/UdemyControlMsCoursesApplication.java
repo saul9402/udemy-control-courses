2f9bc58cc45b196e2c57950ea5e634ae5f9e1e6c
Saul Avila <saul_harry_@hotmail.com>
2021-07-24 19:56:18 -0500

    Se agregan propiedades faltantes a clase y proyecto

--- udemy-control-ms-courses/src/main/java/mx/com/lickodev/udemy/control/courses/UdemyControlMsCoursesApplication.java
+++ udemy-control-ms-courses/src/main/java/mx/com/lickodev/udemy/control/courses/UdemyControlMsCoursesApplication.java
@@ -1,9 +1,15 @@
 package mx.com.lickodev.udemy.control.courses;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableAutoConfiguration
+@EntityScan(basePackages = { "mx.com.lickodev.udemy.control.commons.entity" })
 public class UdemyControlMsCoursesApplication {
 
 	public static void main(String[] args) {
