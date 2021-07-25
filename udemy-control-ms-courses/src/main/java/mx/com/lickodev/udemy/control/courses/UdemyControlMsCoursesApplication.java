36d3377cf04b375f6557cf837cba03270ab85e1b
Saul Avila <saul_harry_@hotmail.com>
2021-07-24 19:38:57 -0500

    Se agrega proyecto nuevo para gestion de cursos

---
+++ udemy-control-ms-courses/src/main/java/mx/com/lickodev/udemy/control/courses/UdemyControlMsCoursesApplication.java
@@ -0,0 +1,13 @@
+package mx.com.lickodev.udemy.control.courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyControlMsCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyControlMsCoursesApplication.class, args);
+	}
+
+}
