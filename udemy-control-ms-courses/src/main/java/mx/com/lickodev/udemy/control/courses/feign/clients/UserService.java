8c7a059b3873138335c91eff29261471a9c5c624
Saul Avila <saul_harry_@hotmail.com>
2021-08-04 21:30:47 -0500

    Se eliminan clases no usadas

--- udemy-control-ms-courses/src/main/java/mx/com/lickodev/udemy/control/courses/feign/clients/UserService.java
+++
@@ -1,9 +0,0 @@
-package mx.com.lickodev.udemy.control.courses.feign.clients;
-
-import mx.com.lickodev.udemy.control.commons.entity.users.User;
-
-public interface UserService {
-
-	User findByUsername(String userName);
-
-}
